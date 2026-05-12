146cfb462c33085d34d009379eb3db03503ddae1
Oscar Martinez <darshuhot@hotmail.com>
2026-05-12 23:17:56 +0200

    feat: add English translations for all project pages

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,8 +2,12 @@
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/en/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/fullstack/geomeasure-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffullstack%2Fgeomeasure-pro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/en/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/fullstack/geomeasure-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Ffullstack%2Fgeomeasure-pro.mdx&astroContentModuleFlag=true")]]);
 		
