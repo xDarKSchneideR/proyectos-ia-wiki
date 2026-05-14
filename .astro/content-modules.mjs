76bf58b6669251892937417bf4ebea3b6414d979
Oscar Martinez <darshuhot@hotmail.com>
2026-05-14 02:20:51 +0200

    fix: add GarantiaBox URL and fix Spanish to Spain Spanish

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,17 +1,17 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")],
-["src/content/docs/en/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/fullstack/geomeasure-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffullstack%2Fgeomeasure-pro.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/herramientas/microfono-obs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fherramientas%2Fmicrofono-obs.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guia-pwa/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguia-pwa%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/en/fullstack/geomeasure-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Ffullstack%2Fgeomeasure-pro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guia-pwa/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguia-pwa%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/en/guia-pwa/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fguia-pwa%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/en/herramientas/microfono-obs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fherramientas%2Fmicrofono-obs.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/en/pwas/constitucion-pro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fconstitucion-pro.mdx&astroContentModuleFlag=true")],
-["src/content/docs/en/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")],
-["src/content/docs/en/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/en/pwas/gestor-turnos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgestor-turnos.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/pwas/garantia-box.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fpwas%2Fgarantia-box.mdx&astroContentModuleFlag=true")]]);
 		
